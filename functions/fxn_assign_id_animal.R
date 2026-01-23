41e61cc5ea75f4d8b50d34ec0a0234a86a16e5d2
Gerard Cramer UMN <gerard@dairycowfootdoc.com>
2026-01-22 20:37:36 -0600

    chanag to herdID

--- functions/fxn_assign_id_animal.R
+++ functions/fxn_assign_id_animal.R
@@ -1,15 +1,17 @@
 library(tidyverse)
 
-fxn_assign_id_animal_default<-function(df){
-  df%>%
-    mutate(id_animal = paste0(source_file_path, '_', ID, '_', BDAT), 
-           id_animal_lact = paste0(source_file_path, '_', ID, '_', BDAT, '_', LACT)
+fxn_assign_id_animal_default <- function(df) {
+  df %>%
+    mutate(
+      id_animal = paste0(HERDID, "_", ID, "_", BDAT),
+      id_animal_lact = paste0(HERDID, "_", ID, "_", BDAT, "_", LACT)
     )
 }
 
-fxn_assign_id_animal_parnell<-function(df){
-  df%>%
-    mutate(id_animal = paste0(str_sub(source_file_path, 18, 47), '_', ID, '_', BDAT), 
-           id_animal_lact = paste0(str_sub(source_file_path, 18, 47), '_', ID, '_', BDAT, '_', LACT)
+fxn_assign_id_animal_parnell <- function(df) {
+  df %>%
+    mutate(
+      id_animal = paste0(str_sub(source_file_path, 18, 47), "_", ID, "_", BDAT),
+      id_animal_lact = paste0(str_sub(source_file_path, 18, 47), "_", ID, "_", BDAT, "_", LACT)
     )
 }
